b5fb530a4e23463aea2375a795730b6a3f3070c9
iamsid47 <siddheshkulthe43@gmail.com>
2023-07-29 11:14:07 +0530

    completed employee id gen

--- server.py
+++ server.py
@@ -1,17 +1,34 @@
 from flask import Flask, request
 import json
+import random
 
 app = Flask(__name__)
 
 def save_employee_data(emp_data):
-    with open("employees.json", "r") as file:
-        existing_data = json.load(file)
+    try:
+        with open("employees.json", "r") as file:
+            existing_data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = []
 
     existing_data.append(emp_data)
 
     with open("employees.json", "w") as file:
         json.dump(existing_data, file, indent=4)
 
+class RandomNumberGenerator:
+    def __init__(self):
+        self.numbers = list(range(1, 1001))
+        random.shuffle(self.numbers)
+
+    def generate_random_number(self):
+        if not self.numbers:
+            self.numbers = list(range(1, 1001))
+            random.shuffle(self.numbers)
+
+        return self.numbers.pop()
+
+rng = RandomNumberGenerator()
 
 # Greeting 
 @app.route("/greeting", methods=['GET'])
@@ -22,8 +39,10 @@ def greeting():
 @app.route('/employee', methods=['POST'])
 def create_employee():
     request_data = request.get_json()
-
+    emp_id = rng.generate_random_number()
+    
     emp_data = {
+        "employeeId": str(emp_id),
         "name": request_data["name"],
         "city": request_data["city"],
     }
